e50d04e00c5ddef3fb01d876d017470cc4fa46b9
Niki1720 <xpaula20@wp.pl>
2023-03-11 21:53:26 +0000

    add association between courses and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+User.create!(email: 'admin@wp.pl', password: 'haslo123', password_confirmation: 'haslo123')
+
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote
+    description: Faker::TvShows::GameOfThrones.quote,
+    user_id: User.first.id
   }])
 end
